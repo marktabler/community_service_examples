dcfc94929a15481ad9545f2f3946a0a71dcc2b92
Mark Tabler <mtabler@newrelic.com>
2014-08-23 17:10:42 -0700

    Add first & last name to User model

--- user_service/db/schema.rb
+++ user_service/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823215811) do
+ActiveRecord::Schema.define(version: 20140824001023) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140823215811) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
